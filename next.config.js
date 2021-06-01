d1abf69eff25612056a909cc211c7e902fd1e2b7
Harry Pham <hung.pham@futurice.com>
2021-06-02 00:57:26 +0300

    Change export to build, added image loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   // Target must be serverless
   target: "serverless",
+  // Image loader needed to be specified for next export to run
+  images: {
+    loader: "imgix",
+    path: "https://noop/",
+},
 };
