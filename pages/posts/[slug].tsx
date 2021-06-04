7049a67c35e2398e7e95eb0551d5b93a96d265fb
Harry Pham <hung.pham@futurice.com>
2021-06-04 11:27:30 +0300

    Disable Github Actions

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -38,14 +38,14 @@ export default function Post({
   return (
     <Layout>
       <Head>
-        <title>{postData?.title}</title>
+        <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData?.title}</h1>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
-          <Date dateString={postData?.date} />
+          <Date dateString={postData.date} />
         </div>
-        <PostPhoto title={postData?.title} url={postData?.coverImage.url} slug={postData?.slug}/>
+        <PostPhoto title={postData.title} url={postData.coverImage.url} slug={postData.slug}/>
         {documentToReactComponents(postData?.content.json)}
       </article>
     </Layout>
